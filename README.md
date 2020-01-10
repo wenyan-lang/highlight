182ed079cadba3fc9318dd08ba5a2f71447e2f8c
Anthony Fu <anthonyfu117@hotmail.com>
2020-01-11 02:03:06 +0800

    chore: add prism stub

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 | --- | --- | --- |
 | [TextMate Grammar](https://macromates.com/manual/en/language_grammars) | [tmGrammar.json](./tmGrammar.json) | [@antfu](https://github.com/antfu) |
 | [CodeMirror](https://codemirror.net/mode/) | [codemirror.js](./codemirror.json) | [@LingDong-](https://github.com/LingDong-) |
-| [Prism](https://prismjs.com/#languages-list) | [prism.js](./prism.js) | [@antfu](https://github.com/antfu) |
+| [Prism](https://prismjs.com/extending.html) 🚧 WIP! | [prism.js](./prism.js) | [@antfu](https://github.com/antfu) |
---
+++ prism.js
@@ -0,0 +1,30 @@
+(function (Prism) {
+	var string = /("|')(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/
+
+	Prism.languages.wenyan = {
+		'comment': /\/\*[\s\S]*?\*\//,
+		'atrule': {
+			pattern: /@[\w-]+[\s\S]*?(?:;|(?=\s*\{))/,
+			inside: {
+				'rule': /@[\w-]+/
+				// See rest below
+			}
+		},
+		'url': {
+			pattern: RegExp('url\\((?:' + string.source + '|[^\n\r()]*)\\)', 'i'),
+			inside: {
+				'function': /^url/i,
+				'punctuation': /^\(|\)$/
+			}
+		},
+		'selector': RegExp('[^{}\\s](?:[^{};"\']|' + string.source + ')*?(?=\\s*\\{)'),
+		'string': {
+			pattern: string,
+			greedy: true
+		},
+		'property': /[-_a-z\xA0-\uFFFF][-\w\xA0-\uFFFF]*(?=\s*:)/i,
+		'important': /!important\b/i,
+		'function': /[-a-z0-9]+(?=\()/i,
+		'punctuation': /[(){};:,]/
+	}
+}(Prism));
